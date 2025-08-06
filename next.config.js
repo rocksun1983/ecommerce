6f0a842e1479d14082de6fd0bfc5016dc85c35ca
rocksun1983 <pagedenterpriseconcept@gmail.com>
2025-08-06 12:57:50 -0700

    Make NEXTAUTH_URL dynamic for Amplify

--- next.config.js
+++ next.config.js
@@ -5,9 +5,12 @@ const nextConfig = {
       {
         protocol: "https",
         hostname: "res.cloudinary.com",
-        pathname: "**", // allow all paths
+        pathname: "**", // allow all paths from Cloudinary
       },
     ],
+  },
+  env: {
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL || "https://main.d9v2opappg0sb.amplifyapp.com",
   },
 };
 
